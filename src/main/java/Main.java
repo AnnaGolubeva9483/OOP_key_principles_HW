5e03f03330f098d1d8fe62483fbf7245a92f60ef
Anna Golubeva <agolubeva9483@gmail.com>
2023-05-24 21:18:08 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Петр";
+        post.passport = "9999 № 999999";
+        post.patronymic = "Степанович";
+        post.phone = "+7 (999)-555-55-55";
+        post.surname = "Петров";
+        post.subscription = true;
+        post.birthday = new FormDate();
+        post.birthday.day = "11";
+        post.birthday.month = "04";
+        post.birthday.year = "2000";
+
+    }
+
+}
